3f8bcbb38c2d34a3e02f32d6465ad0f3c01d2ced
Manoel <142417832+Mvclr@users.noreply.github.com>
2025-06-16 15:29:30 -0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -15,33 +15,18 @@ const __dirname = path.dirname(__filename);
 
 router.use(express.static(path.join(__dirname, "../public")));
 
-router.post('/delete/user', verifyJWTMiddleware,(req, res) =>{
-    const username = req.cookies.username;
-    
-    connection.query(
-        "SELECT id FROM users WHERE username = ?",
-        [username],
-        (err, userRows) => {
-          if (err) {
-            return res.status(500).json({ error: "Erro ao buscar usuário." });
-          }
-          if (userRows.length === 0) {
-            return res.status(404).json({ error: "Usuário não encontrado." });
-          }
-          const userId = userRows[0].id;
-  
-          connection.query(
-            "DELETE FROM users WHERE id = ?", [userId],
-            (err2) => {
-              if (err2) {
-                return res.status(500).json({ error: "Erro ao deletar usuário." });
-              }
-              res.status(200).json({ message: "Usuário deletado com sucesso." });
-
-            }
-          );
-        }
-      );
-})
+router.post('/delete/user', verifyJWTMiddleware, (req, res) => {
+  const username = req.cookies.username
+  connection.query(
+    "DELETE FROM users WHERE username = ?", [username],
+    (err2) => {
+      if (err2) {
+        return res.status(500).json({ error: "Erro ao deletar usuário." });
+      }
+      res.status(200).json({ message: "Usuário deletado com sucesso." });
+    }
+  );
+}
+);
 
 export default router;
