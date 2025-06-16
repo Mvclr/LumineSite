c21b77a5b0709594da48b962131cae383aeffde7
Manoel <142417832+Mvclr@users.noreply.github.com>
2025-06-16 15:51:16 -0300

    Update userRoutes.js
    Deleta também os favoritados do usuário

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,17 +16,38 @@ const __dirname = path.dirname(__filename);
 router.use(express.static(path.join(__dirname, "../public")));
 
 router.post('/delete/user', verifyJWTMiddleware, (req, res) => {
-  const username = req.cookies.username
+  const username = req.cookies.username;
   connection.query(
-    "DELETE FROM users WHERE username = ?", [username],
-    (err2) => {
-      if (err2) {
-        return res.status(500).json({ error: "Erro ao deletar usuário." });
+    "SELECT id FROM users WHERE username = ?", [username],
+    (err, results) => {
+      if (err) {
+        return res.status(500).json({ error: "Erro ao buscar usuário." });
       }
-      res.status(200).json({ message: "Usuário deletado com sucesso." });
+      if (results.length === 0) {
+        return res.status(404).json({ error: "Usuário não encontrado." });
+      }
+
+      const userId = results[0].id;
+      connection.query(
+        "DELETE FROM favorites WHERE user_id = ?", [userId],
+        (err1) => {
+          if (err1) {
+            return res.status(500).json({ error: "Erro ao deletar favoritos do usuário." });
+          }
+
+          connection.query(
+            "DELETE FROM users WHERE id = ?", [userId],
+            (err2) => {
+              if (err2) {
+                return res.status(500).json({ error: "Erro ao deletar usuário." });
+              }
+              res.status(200).json({ message: "Usuário e favoritos deletados com sucesso." });
+            }
+          );
+        }
+      );
     }
   );
-}
-);
+});
 
 export default router;
