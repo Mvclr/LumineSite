03b3f72da40ddf043ce7878c5eb788f85c2b08b6
Manoel <142417832+Mvclr@users.noreply.github.com>
2025-06-16 10:29:01 -0300

    Update userRoute.js
    Rota de deletar o registro de usuário
    Ação realizada diretamente pelo usuário, basta estar logado para realizar a ação
    Sem ação de Admin

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,1 +1,47 @@
+import express from "express";
+import path from "path";
+import bcrypt from "bcrypt";
+import User from "../classes/User.js";
+import connection from "../db/db_server.js";
+import { fileURLToPath } from "url";
+import { verifyJWTMiddleware } from "../controllers/jwtController.js";
+import isLoggedIn from "../controllers/IsLoggedIn.js";
 
+
+const router = express.Router();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+router.use(express.static(path.join(__dirname, "../public")));
+
+router.delete('/delete/user', verifyJWTMiddleware, isLoggedIn, (req, res) =>{
+    const username = req.cookies.username;
+    
+    connection.query(
+        "SELECT id FROM users WHERE username = ?",
+        [username],
+        (err, userRows) => {
+          if (err) {
+            return res.status(500).json({ error: "Erro ao buscar usuário." });
+          }
+          if (userRows.length === 0) {
+            return res.status(404).json({ error: "Usuário não encontrado." });
+          }
+          const userId = userRows[0].id;
+  
+          connection.query(
+            "DELETE FROM users WHERE id = ?", [userId],
+            (err2) => {
+              if (err2) {
+                return res.status(500).json({ error: "Erro ao deletar usuário." });
+              }
+              res.status(200).json({ message: "Usuário deletado com sucesso." });
+
+            }
+          );
+        }
+      );
+})
+
+export default router;
